3955f14465cc362fb5bf3b7f2b7d764b05eef8b6
Alexander Borisov <alexander.borisov@merkle.com>
2023-06-14 17:22:34 +0400

    Simple blog : Add article controller for admin area

--- app/Http/Controllers/Admin/AdminArticlesController.php
+++ app/Http/Controllers/Admin/AdminArticlesController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\CreateArticleRequest;
+use App\Http\Requests\Admin\EditArticleRequest;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Storage;
 
 class AdminArticlesController extends Controller
 {
@@ -12,7 +17,8 @@ class AdminArticlesController extends Controller
      */
     public function index()
     {
-        return view('admin.articles.index');
+        $articles = Article::paginate(20);
+        return view('admin.articles.index', compact('articles'));
     }
 
     /**
@@ -20,46 +26,92 @@ class AdminArticlesController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+
+        return view('admin.articles.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateArticleRequest $request)
     {
-        //
-    }
+        $tags = explode(',', $request->tags);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        if ($request->has('image')) {
+            $filename = time() . '_' . $request->file('image')->getClientOriginalName();
+            $request->file('image')->storeAs('uploads', $filename, 'public');
+        }
+
+        $article = auth()->user()->articles()->create([
+            'title' => $request->title,
+            'image' => $filename ?? null,
+            'article' => $request->article,
+            'category_id' => $request->category
+        ]);
+
+        foreach ($tags as $tagName) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        }
+
+        return redirect()->route('admin.articles.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Article $article)
     {
-        //
+        $categories = Category::all();
+        $tags = $article->tags->implode('name', ', ');
+
+        return view('admin.articles.edit', compact('article', 'categories', 'tags'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(EditArticleRequest $request, Article $article)
     {
-        //
+        $tags = explode(',', $request->tags);
+
+        if ($request->has('image')) {
+            Storage::delete('public/uploads/' . $article->image);
+
+            $filename = time() . '_' . $request->file('image')->getClientOriginalName();
+            $request->file('image')->storeAs('uploads', $filename, 'public');
+        }
+
+        $article->update([
+            'title' => $request->title,
+            'image' => $filename ?? $article->image,
+            'article' => $request->article,
+            'category_id' => $request->category
+        ]);
+
+        $newTags = [];
+        foreach ($tags as $tagName) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            array_push($newTags, $tag->id);
+        }
+        $article->tags()->sync($newTags);
+
+        return redirect()->route('admin.articles.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Article $article)
     {
-        //
+        if ($article->image) {
+            Storage::delete('public/uploads/' . $article->image);
+        }
+
+        $article->tags()->detach();
+        $article->delete();
+
+        return redirect()->route('admin.articles.index');
     }
 }
