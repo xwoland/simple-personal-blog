957a53995043a8dcd5468f9a4894862245930380
Alexander Borisov <alexander.borisov@merkle.com>
2023-06-14 17:23:00 +0400

    Simple blog : Add requests for articles

---
+++ app/Http/Requests/Admin/EditArticleRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'    => 'required',
+            'image'    => 'nullable|image|dimensions:max_width=1000,max_height=1200',
+            'article'  => 'required',
+            'category' => 'required|integer|exists:categories,id',
+            'tags'     => 'required'
+        ];
+    }
+}
